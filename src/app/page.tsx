e0152cabd97db30244df58fb762452f826585be8
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-27 14:32:36 +0300

    refactor: add Megabox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Image from "next/image";
 import style from "@/styles/home.module.sass"
 
 import SeoPart from "@/components/widgets/SEO/SeoPart";
+import Megabox from "@/components/widgets/Megabox/Megabox";
 
 
 export default function Home() {
   return (
       <main className="home_content">
-          
+        <Megabox/>
         <SeoPart/>
       </main>
   );
---
+++ src/components/widgets/Megabox/Megabox.tsx
@@ -0,0 +1,56 @@
+// components/IntroSection.js
+import React from 'react';
+import OrangeButton from './OrangeButton';
+import BlackButton from './BlackButton';
+
+const MegaboxSection = () => {
+    const openLink = (url) => () => {
+        window.location.href = url;
+    };
+
+    return (
+        <div className="flex flex-row items-center w-full min-h-screen">
+            <div className="w-1/2 min-h-screen bg-cover bg-center rounded-lg" style={{ backgroundImage: "url('/assets/img/megaBoxBackground.png')" }}>
+                <p className="text-white text-4xl font-medium leading-tight tracking-tight ml-8 mt-32 w-3/5">
+                    Первый в России<br/>
+                    онлайн-сервис<br/>
+                    по ремонту и строительству<br/>
+                    с фиксированной стоимостью
+                </p>
+            </div>
+
+            <div id="right_part" className="flex flex-col items-center w-1/2 min-h-screen space-y-9">
+                <div className="w-11/12 bg-gray-900 rounded-lg min-h-[57vh]">
+                    <p className="text-white text-2xl font-medium leading-snug tracking-tight ml-8 mt-5">
+                        Воплотим вашу<br/>
+                        мечту в реальность<br/> без визитов на<br/> объект
+                    </p>
+                    <div className="flex flex-row items-center space-x-8 mt-12 ml-8">
+                        <OrangeButton text="Онлайн-калькулятор"/>
+                        <BlackButton text="Стать партнёром"/>
+                    </div>
+                </div>
+
+                <div className="w-11/12 flex flex-col space-y-8">
+                    <button className="bg-gray-800 text-white rounded-lg p-5 text-left" onClick={openLink("services/renovation")}>
+                        <h3 className="text-xl font-medium mb-2">Ремонт</h3>
+                        <p className="hidden_text">Без визитов на объект. <br/>
+                            Заезжайте на готовую <br/> квартиру уже через <br/> 6-8 месяцев</p>
+                    </button>
+                    <button className="bg-gray-800 text-white rounded-lg p-5 text-left" onClick={openLink("services/building")}>
+                        <h3 className="text-xl font-medium mb-2">Строительство</h3>
+                        <p className="hidden_text">Без визитов на объект. <br/>
+                            Заезжайте на готовую <br/> квартиру уже через <br/> 6-8 месяцев</p>
+                    </button>
+                    <button className="bg-gray-800 text-white rounded-lg p-5 text-left" onClick={openLink("partnership")}>
+                        <h3 className="text-xl font-medium mb-2">Партнерская программа</h3>
+                        <p className="hidden_text">Без визитов на объект. <br/>
+                            Заезжайте на готовую <br/> квартиру уже через <br/> 6-8 месяцев</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MegaboxSection;
