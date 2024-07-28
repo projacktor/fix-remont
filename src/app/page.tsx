c3535124e3ae351879c3f779f0246027b182a99d
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-28 19:22:28 +0300

    chore: add new svg directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,21 @@ import style from "@/styles/home.module.sass"
 
 import SeoPart from "@/components/widgets/SEO/SeoPart";
 import Megabox from "@/components/widgets/Megabox/Megabox";
+import TapeSection from "@/components/shared/Tape Section/TapeSection";
 
+import contactIcon from "../../public/assets/svg/TapeSection icons/contactIcon.svg";
 
 export default function Home() {
   return (
       <main className="home_content">
-        <Megabox/>
-        <SeoPart/>
+        <Megabox />
+
+        <div className={`flex flex-row gap-4`}>
+            <TapeSection svg={contactIcon as string} altName={"contactIcon"} text={<p>Без визитов <br/> на объект</p>}/>
+        </div>
+
+        <SeoPart />
+
       </main>
   );
 }
