82185147219fd99f67b213ba547db8b783fac5fe
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-29 17:11:52 +0300

    fix blocks

--- src/components/shared/Hover section/HoverSection.tsx
+++ src/components/shared/Hover section/HoverSection.tsx
@@ -13,14 +13,14 @@ interface HoverSectionProps {
 
 function HoverSection({name, text, link}: HoverSectionProps) {
     return (
-        <button onClick={openLink(link)} className={`${style.revert} h-44 w-[14rem] bg-color-bg-gray flex flex-col items-center gap-1 border rounded-3xl text-color-dark hover:text-white`}>
-            <div className={`flex flex-col items-start gap-2`}>
-                <h3 className="mt-4 max-w-4 leading-6 text-xl font-medium text-left">{name}</h3>
-                <span className={`text-sm text-left font-normal max-w-44`}>
+        <button onClick={openLink(link)} className={`${style.revert} w-[14rem] bg-color-bg-gray px-7 flex flex-col items-start gap-1 border rounded-3xl text-color-dark hover:text-white`}>
+            <div className={`mt-7 flex flex-col items-start gap-2`}>
+                <h3 className="max-w-4 leading-5 text-xl font-medium text-left">{name}</h3>
+                <span className={`text-xs text-left font-normal max-w-44`}>
                     {text}
                 </span>
             </div>
-            <Image alt="arrow" src={arrow as string} width="40" height="40" className={`${style.arrow} relative left-16 bottom-4`}/>
+            <Image alt="arrow" src={arrow as string} width="40" height="40" className={`${style.arrow} relative left-32 bottom-4`}/>
         </button>
     );
 }
--- src/components/widgets/Megabox/Megabox.tsx
+++ src/components/widgets/Megabox/Megabox.tsx
@@ -8,7 +8,7 @@ import HoverSection from "@/components/shared/Hover section/HoverSection";
 function MegaboxSection() {
     return (
         <div className="flex flex-row items-center w-full min-h-screen gap-6">
-            <div className={`${style.bg} max-w-1/2 w-[95svw] min-h-[90svh] bg-cover bg-center rounded-xl`}>
+            <div className={`${style.bg} max-w-1/2 w-[95svw] min-h-[92svh] bg-cover bg-center rounded-xl`}>
                 <p className="text-white text-4xl font-medium leading-tight tracking-tight ml-8 mt-32 w-3/5 relative top-44 left-9">
                     Первый в России <br/>
                     онлайн-сервис <br/>
@@ -17,7 +17,7 @@ function MegaboxSection() {
                 </p>
             </div>
 
-            <div className="flex flex-col gap-2 items-center w-1/2 max-w-1/2">
+            <div className="flex flex-col gap-7 items-center w-1/2 max-w-1/2">
                 <div className="flex flex-col gap-40 p-10 w-full bg-color-dark rounded-3xl">
                     <p className="text-white text-2xl font-medium leading-snug tracking-tight">
                         Воплотим вашу<br/>
@@ -30,9 +30,9 @@ function MegaboxSection() {
                 </div>
 
                 <div className="flex flex-row gap-7">
-                    <HoverSection name="Ремонт" text={<p>Без визитов на объект. <br/>  Заезжайте в готовую <br/> квартиру уже через 6-8 <br/> месяцев</p>} link={""}/>
-                    <HoverSection name="Строительство" text={<p>Без визитов на объект. <br/>  Заезжайте в готовую <br/> квартиру уже через 6-8 <br/> месяцев</p>} link={""}/>
-                    <HoverSection name="Партнерская программа" text={<p>Подходит для физ.лиц, застройщиков, риелторов</p>} link={""}/>
+                    <HoverSection name="Ремонт" text={<p>Без визитов на объект. <br/> Заезжайте в готовую <br/> квартиру уже через <br/> 6-8 месяцев</p>} link={""}/>
+                    <HoverSection name="Строительство" text={<p>Без визитов на объект. <br/>  Заезжайте в готовую <br/> квартиру уже через <br/> 6-8 месяцев</p>} link={""}/>
+                    <HoverSection name="Партнерская программа" text={<p>Подходит для физ.лиц, <br/> застройщиков, <br/> риелторов</p>} link={""}/>
                 </div>
             </div>
         </div>
