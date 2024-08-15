3b109e61aa47b3c918d0157b4dae3e9f5f7ebb01
Arsen <106221641+projacktor@users.noreply.github.com>
2024-08-16 01:06:39 +0300

    fix: make 404 page visible and apply Prettier

--- src/app/pages/gratitude/page.tsx
+++ src/app/pages/gratitude/page.tsx
@@ -1,21 +1,19 @@
 'use client'
-import React from 'react';
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
 import style from './gratitude.module.scss'
 
 import BlackButton from '@/components/shared/buttons/Black Button/BlackButton'
 
-import bg from '../../../../public/assets/img/thankYou.png';
+import bg from '../../../../public/assets/img/thankYou.png'
 
 const Gratitude = () => {
   return (
-    <main
-      className="mb-5 flex w-full items-center justify-center rounded-2xl bg-cover"
-    >
-      <Image src={bg as string} alt={"thank you"} className="w-full h-full relative"/>
+    <main className="mb-5 flex w-full items-center justify-center rounded-2xl bg-cover">
+      <Image src={bg as string} alt={'thank you'} className="relative h-full w-full" />
       <div className="absolute flex flex-col items-center gap-8">
-        <h1 className="heading text-white text-center">
+        <h1 className="heading text-center text-white">
           Благодарим
           <br />
           за обращение!
