cdfda2ddf8830567ca9d9ac9d50f734fd7dcbb2f
Arsen <106221641+projacktor@users.noreply.github.com>
2024-08-19 00:55:55 +0300

    fix: building page

--- src/app/pages/services/building/page.tsx
+++ src/app/pages/services/building/page.tsx
@@ -1,100 +1,117 @@
-import RenovationHeader from '@/components/widgets/RenovationHeader/RenovationHeader'
-import WhiteBullet from '@/components/shared/WhiteBullet/WhiteBullet'
-import SelfRenovation from '@/components/widgets/SelfRenovation/SelfRenovation'
-import ReasonsList from '@/components/widgets/ReasonsList/ReasonsList'
-import Risk from '@/components/widgets/Risk/Risk'
-import Comparison from '@/components/widgets/Comparison/Comparison'
-import FavoriteBusiness from '@/components/widgets/FavoriteBusiness/FavoriteBusiness'
-import WorksGallery from '@/components/widgets/WorksGallery/WorksGallery'
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import creditImg from '../../../../../public/assets/img/creditImg.png'
+import plannerImg1 from '../../../../../public/assets/img/planner img/plannerImg1.png'
+import pseudoVid from '../../../../../public/assets/img/pseudo videos/pseudoVid.png'
+import renovationHeaderBackground from '../../../../../public/assets/img/headBannerBuildingService.png'
+import materialsImg from '../../../../../public/assets/img/creditCardMaterials.png'
+import TapeSection from '@/components/shared/Tape Section/TapeSection'
+import contactIcon from '../../../../../public/assets/svg/TapeSection icons/contactIcon.svg'
+import rublIcon from '../../../../../public/assets/svg/TapeSection icons/rublIcon.svg'
+import shieldIcon from '../../../../../public/assets/svg/TapeSection icons/shiledIcon.svg'
+import lampIcon from '../../../../../public/assets/svg/TapeSection icons/lampIcon.svg'
+import flagIcon from '../../../../../public/assets/svg/TapeSection icons/flagIcon.svg'
+import FavoriteBusiness from '@/components/widgets/Favorite Business/FavoriteBusiness'
+import RiskSlide from '@/components/widgets/Risk Side/RiskSlide'
+import ReasonsList from '@/components/widgets/Reasons List/ReasonsList'
+import GalleryWithFeedback from '@/components/widgets/Gallery with feedback/GalleryWithFeedback'
 import CheckFeedbacks from '@/components/widgets/CheckFeedbacks/CheckFeedbacks'
 import Planner from '@/components/widgets/Planner/Planner'
-import Presentation from '@/components/widgets/Presentation/Presentation'
+import GroupPhoto from '@/components/widgets/GroupPhoto/GroupPhoto'
 import Responsibility from '@/components/widgets/Responsibility/Responsibility'
-import SocialMedia from '@/components/widgets/SocialMedia/SocialMedia'
-import SelfRenovationGallery from '@/components/widgets/SelfRenovationGallery/SelfRenovationGallery'
-import LongBlock from '@/components/widgets/LongBlock/LongBlock'
-import ToBlog from '@/components/widgets/ToBlog/ToBlog'
-import FAQ from '@/components/widgets/FAQ/FAQ'
-import EngConsulting from '@/components/widgets/EngConsulting/EngConsulting'
-import SeoDiv from '@/components/widgets/SeoDiv/SeoDiv'
+import SocialMedia from '@/components/widgets/Social Media/SocialMedia'
+import ToBlog from '@/components/widgets/To blog/ToBlog'
+import FaqSection from '@/components/widgets/Faq Section/FaqSection'
+import Consulting from '@/components/widgets/Consulting/Consulting'
+import SeoPart from '@/components/widgets/SEO/SeoPart'
+import RenovationHeader from '@/components/widgets/Renovation Header/RenovationHeader'
+import Crumb from '@/components/shared/Crumb/Crumb'
+import SelfRenovation from '@/components/widgets/Self Renovation/SelfRenovation'
+import Comparison from '@/components/widgets/Comparison/Comparison'
+import SelfRenovationGallery from '@/components/widgets/Self Renovation Gallery/SelfRenovationGallery'
 
-import renovationHeaderBackground from '@/public/assets/img/headBannerBuildingService.png'
-import contactIco from '@/public/assets/icons/contactIcon.svg'
-import rublIco from '@/public/assets/icons/rublIcon.svg'
-import shieledIco from '@/public/assets/icons/shiledIcon.svg'
-import lampIco from '@/public/assets/icons/lampIcon.svg'
-import flagIco from '@/public/assets/icons/flagIcon.svg'
-import plannerImg1 from '@/public/assets/img/plannerImg1.png'
-import responseYandex from '@/public/assets/img/responseYandex.png'
-import pseudoVid from '@/public/assets/img/pseudoVid.png'
-import responseOtzovik from '@/public/assets/img/responseOtzovik.png'
-import materialsImg from '@/public/assets/img/creditCardMaterials.png'
-
-const BuildingService = () => {
+const RenovationService = () => {
+  const path = ['Услуги', 'Строительство домов']
+  const links = ['/pages/services', '/pages/services/building']
   return (
-    <main className="flex flex-col gap-12">
+    <main className="flex flex-col gap-10">
+      <Crumb path={path} links={links} />
       <RenovationHeader
-        header1={<h1>Строительство домов под ключ по готовым проектам и с фиксированной ценой</h1>}
+        image={renovationHeaderBackground as string}
+        header1={<h1>Ремонт квартир под ключ по готовым проектам и с фиксированной ценой</h1>}
         header2="6 месяцев до готового дома мечты с благоустройством и ремонтом"
-        image={renovationHeaderBackground}
       />
-
-      <div className="flex flex-wrap justify-around gap-4">
-        <WhiteBullet line1="Без визитов" line2="на объект" src={contactIco} />
-        <WhiteBullet line1="100% гарантия фиксированной" line2="сметы" src={rublIco} />
-        <WhiteBullet line1="Все финансовые риски берем" line2="на себя" src={shieledIco} />
-        <WhiteBullet line1="36 готовых" line2="дизайн-проектов" src={lampIco} />
-        <WhiteBullet line1="Все берем на себя" line2="и работаем под ключ" src={flagIco} />
+      <div className={`flex flex-row gap-4`}>
+        <TapeSection
+          svg={contactIcon as string}
+          altName={'contactIcon'}
+          text={
+            <p>
+              Без визитов <br /> на объект
+            </p>
+          }
+        />
+        <TapeSection
+          svg={rublIcon as string}
+          altName={'rublIcon'}
+          text={
+            <p>
+              100% гарантия фиксированной <br /> сметы
+            </p>
+          }
+        />
+        <TapeSection
+          svg={shieldIcon as string}
+          altName={'shieldIcon'}
+          text={
+            <p>
+              Все финансовые риски берём <br /> на себя
+            </p>
+          }
+        />
+        <TapeSection
+          svg={lampIcon as string}
+          altName={'lampIcon'}
+          text={
+            <p>
+              36 готовых <br /> дизнай-проектов
+            </p>
+          }
+        />
+        <TapeSection
+          svg={flagIcon as string}
+          altName={'flagIcon'}
+          text={
+            <p>
+              Всё берем на себя <br /> и работаем под ключ
+            </p>
+          }
+        />
       </div>
 
       <SelfRenovation />
 
       <ReasonsList
         head_text={
-          <h1>
-            <b>
+          <h1 className="heading">
+            <span className="orange-bold">
               Избавим вас от <br /> типичных проблем
-            </b>{' '}
+            </span>{' '}
             <br /> ремонта квартиры:
           </h1>
         }
       />
 
-      <Risk button_text="Рассчитать стоимость" background={materialsImg} />
+      <RiskSlide button_text="Рассчитать стоимость" background={materialsImg} />
 
       <Comparison />
 
       <FavoriteBusiness />
 
-      <div id="home_works_gallery" className="flex flex-col items-center gap-8">
-        <div className="flex flex-col items-center gap-4">
-          <div id="response_names" className="text-center">
-            <p>Но лучше всего о нас расскажут</p>
-            <h1>
-              Выполненные работы <br /> и отзывы клиентов{' '}
-            </h1>
-          </div>
-          <div id="response_bullets" className="flex gap-4">
-            <a
-              className="response_bullet"
-              target="_blank"
-              href="https://yandex.ru/maps/org/fix_remont/110403249402/features/?ll=49.192026%2C55.826770&mode=search&sll=48.744609%2C55.752080&sspn=0.090294%2C0.027834&tab=features&text=%D1%84%D0%B8%D0%BA%D1%81%20%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82&z=12"
-            >
-              <img alt="yandex_picture" src={responseYandex} />
-              <p>
-                <u>Смотреть отзывы на Яндекс</u>
-              </p>
-            </a>
-            <a className="response_bullet response_bullet2" target="_blank">
-              <img alt="otzovik_picture" src={responseOtzovik} />
-              <p>
-                <u>Смотреть отзывы на Отзовик</u>
-              </p>
-            </a>
-          </div>
-        </div>
-        <WorksGallery />
-      </div>
+      <GalleryWithFeedback />
 
       <CheckFeedbacks />
 
@@ -113,7 +130,7 @@ const BuildingService = () => {
         link_text=""
       />
 
-      <Presentation />
+      <GroupPhoto />
 
       <Responsibility />
 
@@ -121,21 +138,19 @@ const BuildingService = () => {
 
       <SelfRenovationGallery />
 
-      <LongBlock />
+      {/*<LongBlock />*/}
 
-      <div className="pseudo_vid">
-        <img alt="pseudo_video" src={pseudoVid} className="h-auto w-full" />
-      </div>
+      <Image src={pseudoVid as unknown as string} alt={'pseudo_vidio'} />
 
       <ToBlog />
 
-      <FAQ />
+      <FaqSection />
 
-      <EngConsulting />
+      <Consulting />
 
-      <SeoDiv />
+      <SeoPart />
     </main>
   )
 }
 
-export default BuildingService
+export default RenovationService
