6d7ef9cae2030716feb9d56cbf450f232a627ce9
Arsen <106221641+projacktor@users.noreply.github.com>
2024-08-21 13:15:04 +0300

    style: apply Prettier

--- src/app/profile/partners/page.tsx
+++ src/app/profile/partners/page.tsx
@@ -14,110 +14,114 @@ import SupportSection from '@/components/widgets/Support Section/SupportSection'
 import banner from '../../../../public/assets/img/bannerTrial.png'
 
 function Home() {
-    const partsData = [
-        {
-            objectName: "Квартира",
-            tariff: Tariff.business,
-            location: "Казань, ул. Волкова",
-            payment: 5032000,
-            part: "Установка полов"
-        },
-        {
-            objectName: "Дом",
-            tariff: Tariff.comfort,
-            location: "Казань, ул. Светлая",
-            payment: 3120000,
-            part: "Заливка фундамента"
-        },
-        {
-            objectName: "Квартира",
-            tariff: Tariff.comfort,
-            location: "Казань, ул. Щапова",
-            payment: 3462000,
-            part: "Объект сдан"
-        }
-        ]
+  const partsData = [
+    {
+      objectName: 'Квартира',
+      tariff: Tariff.business,
+      location: 'Казань, ул. Волкова',
+      payment: 5032000,
+      part: 'Установка полов'
+    },
+    {
+      objectName: 'Дом',
+      tariff: Tariff.comfort,
+      location: 'Казань, ул. Светлая',
+      payment: 3120000,
+      part: 'Заливка фундамента'
+    },
+    {
+      objectName: 'Квартира',
+      tariff: Tariff.comfort,
+      location: 'Казань, ул. Щапова',
+      payment: 3462000,
+      part: 'Объект сдан'
+    }
+  ]
 
-    const clientData = [
-        {
-            objectName: "Квартира",
-            tariff: Tariff.business,
-            location: "Казань, ул. Волкова",
-            payment: 32000,
-            part: "Установка полов"
-        },
-        {
-            objectName: "Дом",
-            tariff: Tariff.comfort,
-            location: "Казань, ул. Светлая",
-            payment: 120000,
-            part: "Заливка фундамента"
-        },
-        {
-            objectName: "Квартира",
-            tariff: Tariff.comfort,
-            location: "Казань, ул. Щапова",
-            payment: 26000,
-            part: "Объект сдан"
-        },
-        {
-            objectName: "Квартира",
-            tariff: Tariff.comfort,
-            location: "Казань, ул. Щапова",
-            payment: 26000,
-            part: "Объект сдан"
-        }
-    ]
+  const clientData = [
+    {
+      objectName: 'Квартира',
+      tariff: Tariff.business,
+      location: 'Казань, ул. Волкова',
+      payment: 32000,
+      part: 'Установка полов'
+    },
+    {
+      objectName: 'Дом',
+      tariff: Tariff.comfort,
+      location: 'Казань, ул. Светлая',
+      payment: 120000,
+      part: 'Заливка фундамента'
+    },
+    {
+      objectName: 'Квартира',
+      tariff: Tariff.comfort,
+      location: 'Казань, ул. Щапова',
+      payment: 26000,
+      part: 'Объект сдан'
+    },
+    {
+      objectName: 'Квартира',
+      tariff: Tariff.comfort,
+      location: 'Казань, ул. Щапова',
+      payment: 26000,
+      part: 'Объект сдан'
+    }
+  ]
   return (
-      <main className="mb-8 flex flex-col gap-8">
-          <Image src={banner as unknown as string} alt="banner" className="w-full"/>
+    <main className="mb-8 flex flex-col gap-8">
+      <Image src={banner as unknown as string} alt="banner" className="w-full" />
 
-          <h1 className="heading">Главная</h1>
+      <h1 className="heading">Главная</h1>
 
-          <div className="flex flex-row justify-between space-x-8">
-              <RefLink/>
+      <div className="flex flex-row justify-between space-x-8">
+        <RefLink />
 
-              <CodeGen/>
-          </div>
+        <CodeGen />
+      </div>
 
-          <Wallet/>
+      <Wallet />
 
-          <div className="flex flex-col items-start space-y-4">
-              <h2 className="heading2">Мои клиенты</h2>
-              <div
-                  className="flex h-max w-full flex-col items-center justify-around space-y-4 rounded-3xl bg-white p-6 overflow-y-auto max-h-96">
-                  {clientData.map((client, index) => (
-                      <React.Fragment key={index}>
-                          <PartsField objectName={client.objectName} tariff={client.tariff} location={client.location} payment={client.payment} part={client.part}/>
-                          {index < clientData.length - 1 && <hr className="w-full border-t border-gray-300"/>}
-                      </React.Fragment>
-                  ))}
-              </div>
-          </div>
+      <div className="flex flex-col items-start space-y-4">
+        <h2 className="heading2">Мои клиенты</h2>
+        <div className="flex h-max max-h-96 w-full flex-col items-center justify-around space-y-4 overflow-y-auto rounded-3xl bg-white p-6">
+          {clientData.map((client, index) => (
+            <React.Fragment key={index}>
+              <PartsField
+                objectName={client.objectName}
+                tariff={client.tariff}
+                location={client.location}
+                payment={client.payment}
+                part={client.part}
+              />
+              {index < clientData.length - 1 && <hr className="w-full border-t border-gray-300" />}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
 
-          <div className="flex flex-col items-start space-y-4">
-              <h2 className="heading2">Мои договоры</h2>
-              <div
-                  className="flex h-max w-full flex-col items-center justify-around space-y-4 rounded-3xl bg-white p-6 overflow-y-auto max-h-96">
-                  {partsData.map((part, index) => (
-                      <React.Fragment key={index}>
-                          <PartsField
-                              objectName={part.objectName}
-                              tariff={part.tariff}
-                              location={part.location}
-                              payment={part.payment}
-                              part={part.part}
-                          />
-                          {index < partsData.length - 1 && <hr className="w-full border-t border-gray-300"/>}
-                      </React.Fragment>
-                  ))}
-              </div>
-          </div>
+      <div className="flex flex-col items-start space-y-4">
+        <h2 className="heading2">Мои договоры</h2>
+        <div className="flex h-max max-h-96 w-full flex-col items-center justify-around space-y-4 overflow-y-auto rounded-3xl bg-white p-6">
+          {partsData.map((part, index) => (
+            <React.Fragment key={index}>
+              <PartsField
+                objectName={part.objectName}
+                tariff={part.tariff}
+                location={part.location}
+                payment={part.payment}
+                part={part.part}
+              />
+              {index < partsData.length - 1 && <hr className="w-full border-t border-gray-300" />}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
 
-          <NewsSection/>
+      <NewsSection />
 
-          <SupportSection/>
-      </main>
+      <SupportSection />
+    </main>
   )
 }
 
