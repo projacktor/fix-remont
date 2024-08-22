3c2460a8d17205934a1dc803b38433cb1a39caaa
Arsen <106221641+projacktor@users.noreply.github.com>
2024-08-22 13:18:39 +0300

    feat: create adding card

---
+++ src/components/modal/New Card/NewCard.tsx
@@ -0,0 +1,150 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { PaymentProvider } from '@/types/Payment Provider'
+import { PaymentFieldProps } from '@/types/PaymentFieldProps'
+
+import mirLogo from '../../../../public/assets/svg/mirLogo.svg'
+import visaLogo from '../../../../public/assets/svg/visaLogo.svg'
+import masterLogo from '../../../../public/assets/svg/mastercardLogo.svg'
+
+interface NewCardProps {
+  onClose: () => void
+  onSave: (newCard: PaymentFieldProps) => void
+}
+
+function NewCard({ onClose, onSave }: NewCardProps) {
+  const [provider, setProvider] = useState<PaymentProvider | null>(null)
+  const [index, setIndex] = useState<string>('') // Изменено на строку
+  const [name, setName] = useState<string>('')
+  const [cvc, setCvc] = useState<string>('') // Добавлено поле для CVC
+
+  const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let cardNumber = e.target.value.replace(/\D/g, '') // Удаление всех символов, кроме цифр
+    cardNumber = cardNumber.slice(0, 16) // Ограничение до 16 цифр
+
+    setIndex(cardNumber)
+
+    if (cardNumber.startsWith('22')) {
+      setProvider(PaymentProvider.Mir)
+    } else if (cardNumber.startsWith('4')) {
+      setProvider(PaymentProvider.Visa)
+    } else if (cardNumber.startsWith('5')) {
+      setProvider(PaymentProvider.MasterCard)
+    } else {
+      setProvider(null)
+    }
+  }
+
+  const handleCvcChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let cvcInput = e.target.value.replace(/\D/g, '') // Удаление всех символов, кроме цифр
+    cvcInput = cvcInput.slice(0, 3) // Ограничение до 3 цифр
+
+    setCvc(cvcInput)
+  }
+
+  function handleSave() {
+    if (provider && index && name && cvc.length === 3) {
+      const cardIndex = Number(index.slice(-4)) // Присваивание последних 4 цифр
+      onSave({ provider, index: cardIndex, name })
+      onClose()
+    } else {
+      alert('Пожалуйста, заполните все поля.')
+    }
+  }
+
+  return (
+    <div className="fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-color-dark bg-opacity-75">
+      <form className="flex h-max w-max flex-col space-y-5 rounded-3xl bg-white p-5">
+        <h3 className="heading2 w-full text-start">Добавить новую карту</h3>
+
+        <div className="flex flex-col space-y-3">
+          <div className="flex items-center space-x-5">
+            {provider && (
+              <Image
+                src={
+                  provider === PaymentProvider.Mir
+                    ? (mirLogo as string)
+                    : provider === PaymentProvider.Visa
+                      ? (visaLogo as string)
+                      : (masterLogo as string)
+                }
+                alt={`${provider} logo`}
+                width={40}
+              />
+            )}
+            <label className="text-xs font-normal text-gray-400" htmlFor="number">
+              Номер карты:
+            </label>
+          </div>
+          <input
+            type="text" // Изменено на текст
+            placeholder="Card number"
+            className="w-full rounded-3xl bg-color-bg-gray p-3 placeholder:text-gray-500"
+            value={index}
+            id="number"
+            onChange={handleCardNumberChange}
+          />
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <label className="text-xs font-normal text-gray-400" htmlFor="name">
+            Имя карты:
+          </label>
+          <input
+            type="text"
+            placeholder="Card name"
+            className="w-full rounded-3xl bg-color-bg-gray p-3 placeholder:text-gray-500"
+            value={name}
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-row space-x-5">
+          <div className="flex flex-col space-y-3">
+            <label className="text-xs font-normal text-gray-400" htmlFor="date">
+              Срок карты:
+            </label>
+            <input
+              type="date"
+              placeholder="Card name"
+              className="w-full rounded-3xl bg-color-bg-gray p-3 placeholder:text-gray-500"
+              id="date"
+            />
+          </div>
+          <div className="flex flex-col space-y-3">
+            <label className="text-xs font-normal text-gray-400" htmlFor="CVC">
+              СVC:
+            </label>
+            <input
+              type="text" // Изменено на текст
+              placeholder="CVC"
+              className="w-full rounded-3xl bg-color-bg-gray p-3 placeholder:text-gray-500"
+              value={cvc}
+              id="CVC"
+              onChange={handleCvcChange}
+            />
+          </div>
+        </div>
+        <div className="flex h-12 w-full flex-row justify-between space-x-5">
+          <button
+            type="button"
+            className="flex w-44 items-center justify-center rounded-full bg-color-orange px-8 py-6 font-semibold text-white"
+            onClick={handleSave}
+          >
+            Сохранить
+          </button>
+          <button
+            type="button"
+            className="flex w-44 items-center justify-center rounded-full border border-gray-900 bg-transparent px-4 py-2 text-base font-semibold text-gray-900"
+            onClick={onClose}
+          >
+            Отмена
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default NewCard
--- src/components/widgets/Payment Cards/PaymentCards.tsx
+++ src/components/widgets/Payment Cards/PaymentCards.tsx
@@ -4,6 +4,7 @@ import { PaymentFieldProps } from '@/types/PaymentFieldProps'
 
 import PaymentField from '@/components/shared/Payment Field/PaymentField'
 import { PaymentProvider } from '@/types/Payment Provider'
+import NewCard from '@/components/modal/New Card/NewCard'
 
 function PaymentCards() {
   const [data, setData] = useState([
@@ -29,13 +30,19 @@ function PaymentCards() {
     }
   ])
 
+  const [isPopupOpen, setPopupOpen] = useState(false)
+
   const removeCard = (indexToRemove: number) => {
     setData((prevData) => prevData.filter((_, index) => index !== indexToRemove))
+  }
+
+  const addCard = (newCard: PaymentFieldProps) => {
+    setData((prevData) => [...prevData, newCard])
   }
   return (
     <div className="flex flex-col space-y-6">
       <h2 className="heading2">Способы оплаты</h2>
-      <div className="space-y-8 rounded-3xl bg-white p-5">
+      <div className="rounded-3xl bg-white p-5">
         <div className="flex max-h-72 flex-col overflow-y-auto">
           {data.length === 0 ? (
             <p className="w-full text-center font-medium text-gray-400">
@@ -55,13 +62,17 @@ function PaymentCards() {
             ))
           )}
         </div>
-        <div className="flex flex-row justify-between px-4">
+        <div className="flex flex-row justify-between px-4 py-4">
           <h5 className="headingStruct text-lg">Добавить новый способ вывода</h5>
-          <button className="rounded-xl bg-color-orange px-3 py-1 text-3xl font-medium text-white">
+          <button
+            className="rounded-xl bg-color-orange px-3 py-1 text-3xl font-medium text-white"
+            onClick={() => setPopupOpen(true)}
+          >
             +
           </button>
         </div>
       </div>
+      {isPopupOpen && <NewCard onClose={() => setPopupOpen(false)} onSave={addCard} />}
     </div>
   )
 }
