43bb3b43829f24b17d224ab688842ff2c77f21b8
Arsen <106221641+projacktor@users.noreply.github.com>
2024-08-24 12:41:36 +0300

    feat: add Card component

---
+++ src/components/shared/Card/Card.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+function Card({ title, price }) {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked)
+  }
+
+  return (
+    <div
+      className={`rounded-lg border-4 p-4 ${isChecked ? 'border-color-orange' : 'border-gray-200'}`}
+    >
+      <div className="relative">
+        <div
+          className={`h-40 w-full rounded-lg bg-gray-100 ${
+            isChecked ? 'border-4 border-color-orange' : 'border-4 border-transparent'
+          }`}
+        ></div>
+
+        <div className="mt-4 flex items-center">
+          <input
+            type="checkbox"
+            id="checkbox"
+            className="form-checkbox h-4 w-4 rounded border-gray-300 checked:border-none"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            style={{
+              backgroundColor: isChecked ? '#FFA500' : 'transparent',
+              borderColor: isChecked ? '#FFA500' : '#E5E7EB'
+            }}
+          />
+          <label htmlFor="checkbox" className="ml-2 text-lg font-medium text-black">
+            {title}
+          </label>
+        </div>
+
+        <p className="mt-2 text-gray-500">{price} руб/м²</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
