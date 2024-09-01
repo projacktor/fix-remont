d74dd3a3b0b1be8ac5614c83695ca2da7b649ec3
Projacktor <1234xl.com@gmail.com>
2024-09-01 23:18:42 +0300

    refactor: update Planner widget and props in components

--- src/components/widgets/Planner/Planner.tsx
+++ src/components/widgets/Planner/Planner.tsx
@@ -1,36 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+
+interface Step {
+  description: string
+  time: string
+  newP2Text: string
+}
 
 interface PlannerProps {
   planner_img: string
   time_text: string
   p1_text: string
-  step1_description: string
-  step1_time: string
-  step2_description: string
-  step2_time: string
-  step3_description: string
-  step3_time: string
-  step4_description: string
+  steps: Step[]
   p2_text: string
   link_text: string
 }
 
-function Planner({
-  planner_img,
-  time_text,
-  p1_text,
-  step1_description,
-  step1_time,
-  step2_description,
-  step2_time,
-  step3_description,
-  step3_time,
-  step4_description,
-  p2_text,
-  link_text
-}: PlannerProps) {
+function Planner({ planner_img, time_text, p1_text, steps, p2_text, link_text }: PlannerProps) {
+  const [currentP2Text, setCurrentP2Text] = useState(p2_text)
+
   return (
     <section className="flex w-full flex-row justify-between rounded-3xl bg-color-dark px-3 py-16">
       <div className="flex w-1/2 flex-col justify-between gap-6 pl-12">
@@ -40,27 +29,21 @@ function Planner({
           </h2>
           <p className="font-normal">отделяют вас от {p1_text}</p>
         </div>
-        <div className="flex min-h-[80%] w-4/5 flex-col justify-around gap-4 self-stretch">
-          {[step1_description, step2_description, step3_description, step4_description].map(
-            (desc, index) => (
-              <div
-                key={index}
-                className="flex h-20 w-full items-center justify-between rounded-2xl border border-gray-300 px-10 text-white opacity-30 transition-opacity hover:bg-white hover:text-black hover:opacity-100"
-              >
-                <p className="text w-64 font-medium">{desc}</p>
-                <p className="text font-medium">
-                  {[step1_time, step2_time, step3_time][index] &&
-                    `${[step1_time, step2_time, step3_time][index]} мин`}
-                </p>
-              </div>
-            )
-          )}
-          <div className="mt-12">
-            <Link href="#" className="font-semibold text-color-orange underline">
-              {link_text}
-            </Link>
-          </div>
+        <div className="flex min-h-[60%] w-4/5 flex-col justify-around gap-4 self-stretch">
+          {steps.map((step, index) => (
+            <div
+              key={index}
+              className="flex h-20 w-full cursor-pointer items-center justify-between rounded-2xl border border-gray-300 p-11 text-white opacity-30 transition-opacity hover:bg-white hover:text-black hover:opacity-100"
+              onClick={() => setCurrentP2Text(step.newP2Text)} // Обновляем p2_text при клике
+            >
+              <p className="text w-64 font-medium">{step.description}</p>
+              <p className="text font-medium">{step.time && `${step.time} мин`}</p>
+            </div>
+          ))}
         </div>
+        <Link href="#" className="font-semibold text-color-orange underline">
+          {link_text}
+        </Link>
       </div>
       <div className="flex w-1/2 flex-col items-start justify-center">
         <Image
@@ -68,7 +51,7 @@ function Planner({
           alt="FIX-remont_planner"
           className="w-11/12"
         />
-        <p className="mt-4 text-2xl font-medium text-white">{p2_text}</p>
+        <p className="mt-4 pr-8 text-2xl font-medium text-white">{currentP2Text}</p>
       </div>
     </section>
   )
