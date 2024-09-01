2f754283f651797fd99a52cc3936983e7433346b
Projacktor <1234xl.com@gmail.com>
2024-09-01 22:01:03 +0300

    fix: styling at ReasonBlock

--- src/components/shared/Reason Block/ReasonBlock.tsx
+++ src/components/shared/Reason Block/ReasonBlock.tsx
@@ -8,11 +8,11 @@ interface ReasonBlockProps {
 
 const ReasonBlock: React.FC<ReasonBlockProps> = ({ text1, text2, description }) => {
   return (
-    <div className="w-[660px] rounded-3xl bg-black p-8 text-white even:bg-white even:text-black">
-      <h2 className="heading2">
+    <div className="w-full rounded-3xl bg-color-dark p-12 text-white even:bg-white even:text-black">
+      <h2 className="headingStruct text-4xl">
         {text1} <br /> {text2}
       </h2>
-      <p className="mt-4 max-w-full text-base md:max-w-[590px] md:text-lg">{description}</p>
+      <p className="mt-4 max-w-full text-lg">{description}</p>
     </div>
   )
 }
