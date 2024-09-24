a43022ed4cb22a4d1d9ec21f9f03cf26ffa88484
Projacktor <1234xl.com@gmail.com>
2024-09-24 12:26:18 +0300

    refactor: add new aims to todo.txt

---
+++ src/components/shared/Feedback Box/FeedbackBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface FeedbackBoxProps {
+  image: string
+  name: string
+  text: string
+}
+
+function FeedbackBox({ image, name, text }: FeedbackBoxProps) {
+  return (
+    <div className="bg-white"></div>
+  )
+}
+
+export default FeedbackBox
